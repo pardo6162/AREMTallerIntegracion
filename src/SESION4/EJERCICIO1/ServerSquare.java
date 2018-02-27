efbe5df040bc4765e18eb957f8f4228e984c356a
Andres Felipe Pardo Mesa <a.pardo6162@gmail.com>
2018-02-27 16:17:02 -0500

    Modelo Cliente-Servidor 	comunicacion via socket

--- src/SESION4/EJERCICIO1/ServerSquare.java
+++ src/SESION4/EJERCICIO1/ServerSquare.java
@@ -5,12 +5,47 @@
  */
 package SESION4.EJERCICIO1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author 2115237
  */
 public class ServerSquare {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(
+                    clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        int exp=2;
+        while((inputLine = in.readLine())!=null){
+            System.out.println("Recibido: " + inputLine);
+            outputLine = "Respuesta: " + Math.pow(Integer.parseInt(inputLine),exp) ;
+            out.println(outputLine);
+        }
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
